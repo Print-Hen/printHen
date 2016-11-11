0b1a432f7ceebb144395698ea2ba4c90d68b1e6e
Raghuram Iyer <raghuram8892@gmail.com>
2016-11-11 15:00:16 +0530

    1.1

--- printhen/views.py
+++ printhen/views.py
@@ -14,11 +14,12 @@ from django.conf import settings
 from django.http import HttpResponse
 def index(request):
     try:
-        with open('credentials.json') as data_file:
-            data = json.load(data_file)
+        with open('/home/pi/printhen/credentials.json') as data_file:
+           data = json.load(data_file)
         pprint(data)
-    except:
-        return HttpResponseRedirect("credentialserror")
+    except Exception,err:
+        print err
+        return
     host = data["imap_hostname"]
     user = data["username"]
     password = data["password"]
@@ -38,46 +39,54 @@ def index(request):
         else:
             print "MAGIC TITLE FAILED"
             printhen_response(data["username"], from_addr, "[no-reply]PRINTHEN-INVALID SUBJECT", "Hey buddy kindly send mail with PRINTHEN as subject in order to initiate the print")
+            return
+
+
         from_addr = from_addr[from_addr.find("<")+1:from_addr.find(">")]
         print "====================="
         print "NEW MAIL "
         print "====================="
         print "from    : " + from_addr
         print "Message : " + body
-        if "begin print" not in body or "end print" not in body or "from" not in body or "to" not in body or "copies" not in body:
-            printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN. SYNTAX ERROR", "Syntax error. The correct syntax is: begin print from x to y copies z end print")
-        else:
-            body = body[body.find("begin print")+11:body.find("end print")]
-            op = parseBody(body)
-            conn = cups.Connection()
-            print body
-            print op
-            if not mail.attachments:
-                printhen_response(data["username"], from_addr, "[no-reply]PRINTHEN-NO ATTACHMENT FOUND", "Hey Buddy, I guess you forgot to attach a document for printing")
-            if int(op['from']) > int(op['to']):
-                printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN-START PAGE GREATER THAN END PAGE", "hey buddy it seems like the from value is greater that to value kindly check it")
-            for attachment in mail.attachments:
-                filename = settings.MEDIA_PATH + attachment[0]
-                with open(filename, 'wb') as f:
-                    f.write(attachment[1])
-                #conn = cups.Connection()
-                options = {}
-                options['copies'] = op['copies']
-                s = []
-                s.append(op['from'])
-                s.append("-")
-                s.append(op['to'])
-                s1 = ''.join(s)
-                options['page-ranges'] = s1
-                print options
-                printers = conn.getPrinters()
-                for printer in printers:
-                    print printers.items()
-                    print printer, printers[printer]["device-uri"]
-                    conn.printFile(printers[printer]["printer-info"], filename, "print", options)
-    return HttpResponse("Hello")
+        # if "begin print" not in body or "end print" not in body or "from" not in body or "to" not in body or "copies" not in body:
+        #     printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN. SYNTAX ERROR", "Syntax error. The correct syntax is: begin print from x to y copies z end print")
+        #     return
 
+        # else:
+        #body = body[body.find("begin print")+11:body.find("end print")]
+        op = parseBody(body)
+        conn = cups.Connection()
+        print body
+        print op
+        if not mail.attachments:
+            printhen_response(data["username"], from_addr, "[no-reply]PRINTHEN-NO ATTACHMENT FOUND", "Hey Buddy, I guess you forgot to attach a document for printing")
+            return
 
+        if int(op['from']) > int(op['to']):
+            printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN-START PAGE GREATER THAN END PAGE", "hey buddy it seems like the from value is greater that to value kindly check it")
+            return
+        for attachment in mail.attachments:
+            filename = settings.MEDIA_PATH + attachment[0]
+            with open(filename, 'wb') as f:
+                f.write(attachment[1])
+            #conn = cups.Connection()
+            options = {}
+            options['copies'] = op['copies']
+            s = []
+            s.append(op['from'])
+            s.append("-")
+            s.append(op['to'])
+            s1 = ''.join(s)
+            options['page-ranges'] = s1
+            print options
+            printers = conn.getPrinters()
+            for printer in printers:
+                print printers.items()
+                print printer, printers[printer]["device-uri"]
+                conn.printFile(printers[printer]["printer-info"], filename, "print", options)
+    print "SUCCESS"
+    return
+    
 def printhen_response(from_addr, to_addr, subject, msg):
     msg1 = MIMEText(msg)
     msg1['Subject'] = subject
@@ -97,8 +106,11 @@ def printhen_response(from_addr, to_addr, subject, msg):
     s.quit()
 
 def parseBody(body):
-    cmd = body.split()
-    d = dict(itertools.izip_longest(*[iter(cmd)] * 2, fillvalue=""))
+    printhen_nltk.extract_information(body)
+    d  = {}
+    d['from'] = printhen_nltk.getFrom()
+    d['to'] = printhen_nltk.getTo()
+    d['copies']= printhen_nltk.getCopies()
     return d
 
 @csrf_exempt
