c429d73e73f672ad3a7597130863b5956911be32
Raghuram Iyer <raghuram8892@gmail.com>
2016-11-26 18:58:24 +0530

    11

--- printhen/views.py
+++ printhen/views.py
@@ -14,7 +14,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.http import HttpResponse
 def index(request):
-try:
+    try:
         with open('/home/pi/printhen/credentials.json') as data_file:
            data = json.load(data_file)
         pprint(data)
@@ -140,55 +140,7 @@ try:
         printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN PRINT SUCCESS","Your Print has been successfully done.")
                     
     #printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN",str(op))
-    return HttpResponse("YEAH")
-
-
-        from_addr = from_addr[from_addr.find("<")+1:from_addr.find(">")]
-        print "====================="
-        print "NEW MAIL "
-        print "====================="
-        print "from    : " + from_addr
-        print "Message : " + body
-        # if "begin print" not in body or "end print" not in body or "from" not in body or "to" not in body or "copies" not in body:
-        #     printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN. SYNTAX ERROR", "Syntax error. The correct syntax is: begin print from x to y copies z end print")
-        #     return
-
-        # else:
-        #body = body[body.find("begin print")+11:body.find("end print")]
-        op = parseBody(body)
-        conn = cups.Connection()
-        print body
-        print op
-        if not mail.attachments:
-            printhen_response(data["username"], from_addr, "[no-reply]PRINTHEN-NO ATTACHMENT FOUND", "Hey Buddy, I guess you forgot to attach a document for printing")
-            return
-
-        if int(op['from']) > int(op['to']):
-            printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN-START PAGE GREATER THAN END PAGE", "hey buddy it seems like the from value is greater that to value kindly check it")
-            return
-        for attachment in mail.attachments:
-            filename = settings.MEDIA_PATH + attachment[0]
-            with open(filename, 'wb') as f:
-                f.write(attachment[1])
-            #conn = cups.Connection()
-            options = {}
-            options['copies'] = op['copies']
-            s = []
-            s.append(op['from'])
-            s.append("-")
-            s.append(op['to'])
-            s1 = ''.join(s)
-            options['page-ranges'] = s1
-            print options
-            printers = conn.getPrinters()
-            for printer in printers:
-                print printers.items()
-                print printer, printers[printer]["device-uri"]
-                conn.printFile("printhen", filename, "print", options)
-                print "SUCCESS"
-    #printhen_response(data["username"], from_addr, "[no-reply] PRINTHEN",str(op))
-            
-    return HttpResponse("SUCCESS");
+    return
     
 def printhen_response(from_addr, to_addr, subject, msg):
     msg1 = MIMEText(msg)
