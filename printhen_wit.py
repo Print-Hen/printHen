da9018e2023bc92e048f800ecf487e7742a34970
Raghuram Iyer <raghuram8892@gmail.com>
2016-12-07 18:46:25 +0530

    added one sided

--- printhen_wit.py
+++ printhen_wit.py
@@ -22,10 +22,12 @@ def send(request, response):
     print(response['text'])
 
 def extract_value(request):
+    print "extract value executing"
     context = request['context']
     entities  = request['entities']
     copies = first_entity_value(entities,'copies',0)
     page = first_entity_value(entities,'page',0)
+    onesided = first_entity_value(entities,'oneside',0)
     if copies:
         context['copies'] = str(copies)
     else:
@@ -36,30 +38,42 @@ def extract_value(request):
         
     else:
         context['page'] = str(first_entity_value(entities,'number',0))
+    if onesided:
+        context['onesided'] = True
+    else:
+        context['onesided'] = False
     return context
 
 def extract_value_whole_doc(request):
+    print "extract Whole doc executing"
     context = request['context']
     entities  = request['entities']
     copies = first_entity_value(entities,'copies',0)
+    onesided = first_entity_value(entities,'oneside',0)
     if copies:
         context['copies'] = str(copies)
     else:
         context['copies'] = '1'
      
         context['page'] = '-1'
+    if onesided:
+        context['onesided'] = True
+    else:
+        context['onesided'] = False
     return context
     
 def extract_value_to_from(request):
+    print "extract from to  executing"
     context = request['context']
     entities = request['entities']
-    print "context"
-    print context
-    print "entities"
-    print entities
+    #print "context"
+    #print context
+    #print "entities"
+    #print entities
     copies = first_entity_value(entities,'copies',0)
     from_page = first_entity_value(entities,'from',0)
     to_page = first_entity_value(entities,'to',0)
+    onesided = first_entity_value(entities,'oneside',0)
     # loc = first_entity_value(entities, 'location')
     # if loc:
     #     context['forecast'] = 'sunny'
@@ -84,6 +98,10 @@ def extract_value_to_from(request):
     else:
         context['to_page'] = str(first_entity_value(entities,'number',1))
     
+    if onesided:
+        context['onesided'] = True
+    else:
+        context['onesided'] = False
     return context
 
 
